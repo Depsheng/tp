6bccdc0f9491f87574f881f08fb032d802674390
Depsheng <jiesheng.yeo@gmail.com>
2026-03-13 10:43:47 +0800

    Edit import order
    Changed the order of the imports to fit the lexicographical order to pass checkStyle

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -5,8 +5,8 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
