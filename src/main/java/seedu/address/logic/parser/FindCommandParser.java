30ad93eca961030e10bfa314abf0cbf388ae5af3
Melville Lee <Melville.leesy@gmail.com>
2026-04-11 19:59:02 +0800

    Make styling changes to fit style guide

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -2,7 +2,11 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.FindCommand;
